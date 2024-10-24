cb6c35627818a2fc069e5a381b7987cd3f530ef3
shanebarrios <sbarrio1@terpmail.umd.edu>
2024-10-24 16:59:30 -0400

    more documentation

--- app.py
+++ app.py
@@ -5,23 +5,25 @@ from courses import Course, Section
 import schedule_builder_funcs as sb
 
 app = Flask(__name__)
-app.secret_key = "55029ed1d113712628567fa4fc2e9b00bc92c5cafc7965335c86aa9664c1c981"
+with open("secret.txt", 'r') as file:
+    app.secret_key = file.read().strip()
 
 all_course_data = utils.read_data_from_json("data/course_data_202501.json")
-uniq_course_names = set()
+uniq_course_names = set() # Making sure identical course names are not entered
 
-name_i = 0
-
+# Defines how the app reacts to input from the user
 @app.route("/submit", methods=['POST'])
 def course_submit():
     courses = []
     sections = []
     uniq_course_names = set()
+    # Read all course/section names that were inputted
     for i in range(1, 8): 
         name = request.form.get(f"course{str(i)}")
         if not name or len(name) < 2:
             continue
         course_name = (name.split("-", 1))[0].upper()
+        # No repeats allowed
         if course_name in uniq_course_names:
             return redirect(url_for('index', error_msg=f"{course_name} repeated"))
         try:
@@ -40,21 +42,25 @@ def course_submit():
     reqs = {"pace": pace_dic[request.form.get("pace")],
             "earliest_start": int(request.form.get("earliest-start")),
             "latest_end": int(request.form.get("latest-end"))}
+    # Don't perform any algorithm if nothing inputted
     if len(courses) == 0 and len(sections) == 0:
         return redirect(url_for('index'))
+    # Get all valid schedules
     schedules = sb.get_valid_schedules(sections=sections, courses=courses, reqs=reqs)
     if len(schedules) == 0:
         return redirect(url_for('index', error_msg="No valid schedule found"))
     sorted_n = sb.get_n(sb.sort_highest_rated(schedules), 100)
+    # Serialize the top 100 highest rated schedules
     session["schedules"] = pickle.dumps(sorted_n)
-    # error check if none
     return redirect(url_for('show_schedule', index=0))
 
+# Home screen of the website, optional error message
 @app.route("/", methods=['GET'])
 def index():
     error_msg = request.args.get("error_msg")
     return render_template("hello.html", error_msg=error_msg)
 
+# Shows a different schedule based on the index
 @app.route("/view/<int:index>")
 def show_schedule(index):
     schedules = pickle.loads(session.get("schedules", pickle.dumps([])))
--- schedule_builder_funcs.py
+++ schedule_builder_funcs.py
@@ -7,6 +7,9 @@ JOGGING_PACE = 6
 BIKING_SCOOTER_PACE = 10
 LITERALLY_TELEPORTING_PACE = 99999999
 
+# Reads a particular course or section id from the data
+# If found, returns a Section or Course object
+# If not found, raises a KeyError
 def read_course(input, data):
     input = input.upper()
     separated = input.split("-", 1)
@@ -24,6 +27,8 @@ def read_course(input, data):
         new_course = Course(course_id=course_str, data=course_data)
         return new_course
 
+# Checks if two sections have overlapping meetings,
+# based on the movement pace of the user
 @lru_cache(maxsize=None)
 def has_overlapping_meetings(s1, s2, pace):
     for i in range(5):
@@ -33,12 +38,16 @@ def has_overlapping_meetings(s1, s2, pace):
                     return True
     return False
 
+# Checks if a section overlaps the schedule
+# based on the movement pace of the user
 def overlaps_schedule(s1, schedule, pace):
     for s2 in schedule:
         if has_overlapping_meetings(s1, s2, pace):
             return True
     return False
 
+# Checks if a section overlaps any of the blocks with form
+# (start_time, end_time)
 @lru_cache(maxsize=None)
 def overlaps_blocks(s1, blocks):
     if blocks is None:
@@ -50,6 +59,7 @@ def overlaps_blocks(s1, blocks):
                     return True
     return False
 
+# Checks to make sure that the section meets all the requirements
 def meets_reqs(section, reqs):
     if section.open_seats == 0:
         return False
@@ -57,6 +67,12 @@ def meets_reqs(section, reqs):
         return False
     return True
 
+# Recursive backtracking algorithm to find all possible combinations of sections
+# We must have exactly one section from each course
+# In addition, no section's meetings can overlap another section's meetings and all
+# sections must meet the requirements specified in reqs
+#
+# reqs is a dictionary with keys for "pace", "earliest_start", "latest_end", and "blocks"
 def get_valid_backtrack(courses, reqs, cur_schedule, valid_schedules, course_index):
     if course_index == len(courses):
         valid_schedules.append(cur_schedule[:])
@@ -69,12 +85,13 @@ def get_valid_backtrack(courses, reqs, cur_schedule, valid_schedules, course_ind
             get_valid_backtrack(courses, reqs, cur_schedule, valid_schedules, course_index+1)
             cur_schedule.pop()
 
+# Gets all valid schedules based on desired courses, sections, and requirements
 def get_valid_schedules(courses, sections, reqs):
     schedules = []
     get_valid_backtrack(courses, reqs, sections, schedules, 0)
     return schedules
 
-
+# Gets the average professor rating for a schedule, weighted by credits per section
 def get_average_rating_weighted(sections):
     rating_tot = 0
     credits_tot = 0
@@ -83,9 +100,11 @@ def get_average_rating_weighted(sections):
         credits_tot += section.credits
     return round(rating_tot/credits_tot, 3)
 
+# Gets the first n schedules from a list of schedules
 def get_n(schedules, n):
     return schedules[:n]
 
+# Sorts a list of schedules by average professor rating, highest to lowest
 def sort_highest_rated(schedules):
     return sorted(schedules, key=get_average_rating_weighted, reverse=True)
 
